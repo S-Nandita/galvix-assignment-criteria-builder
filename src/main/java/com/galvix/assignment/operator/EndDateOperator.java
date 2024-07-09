38a9d520abbea5b721b678a948f67b04bd18846c
s-nandita <snandita2023@gmail.com>
2024-07-09 12:27:52 +0530

    minor changes

---
+++ src/main/java/com/galvix/assignment/operator/EndDateOperator.java
@@ -0,0 +1,10 @@
+package com.galvix.assignment.operator;
+
+public enum EndDateOperator {
+    IS,
+    IS_NOT,
+    IS_AFTER,
+    ON_OR_AFTER,
+    IS_BEFORE,
+    ON_OR_BEFORE
+}
---
+++ src/main/java/com/galvix/assignment/operator/ServiceNameOperator.java
@@ -0,0 +1,7 @@
+package com.galvix.assignment.operator;
+
+public enum ServiceNameOperator {
+    IS,
+    IS_NOT,
+    IS_ANY_OF
+    }
